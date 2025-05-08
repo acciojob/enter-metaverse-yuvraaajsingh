54b9b0acd9c6936e373d94e96e72c6973315d0c0
Yuvraj Singh <84833138+yuvraaajsingh@users.noreply.github.com>
2025-05-08 13:47:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-//your JS code here. If required.
 function clicked() {
-    let p = document.getElementById("status");
-    let h = document.createElement("h1");
-    h.innerText = "Entered Metaverse";
-    p.parentNode.replaceChild(h, p);
+    document.getElementById("status").innerHTML=`<h1>Entered Metaverse</h1>`
 }
