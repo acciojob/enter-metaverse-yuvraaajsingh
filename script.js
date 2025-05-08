9fa7e68cd30eb78d4611d009695c3a1ce4d80749
Yuvraj Singh <84833138+yuvraaajsingh@users.noreply.github.com>
2025-05-08 13:24:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function clicked() {
+    let p = document.getElementById("status");
+    let h = document.createElement("h1");
+    h.innerText = "Entered Metaverse";
+    p.parentNode.replaceChild(h, p);
+}
